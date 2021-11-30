9a911789b3b15d14cc9845864b6e683a8b8289be
Sergio Rubiano <sergiolovesbooks@gmail.com>
2021-11-30 15:24:43 -0500

    Actualizacion 2

--- Condicionales/ejercicio1/ej.js
+++ Condicionales/ejercicio1/ej.js
@@ -2,11 +2,13 @@ let h1resultado= document.querySelector("#resultado")
 
 let altura= Number (prompt("A que altura desa que llegue el proyectíl"))
 let velocidad= Number (prompt("Velocidad inicia del momento que lo lanza"))
+let t= (0-velocidad)/(0- 9.8)
+let desplazamiento= (velocidad*t)-((9.8*t*t)/2)
 
-if(altura <= velocidad){
+if(altura <= desplazamiento){
     resultado.innerHTML= "Llega a la altura deseada"
 }else{
-    if(altura> velocidad){
+    if(altura> desplazamiento){
         resultado.innerHTML= "No llega a la altura deseada"
     }
 }
