ccebc83052db3a02081ddfa6f9c32a0cc62b104e
Serginator99 <92330120+Serginator99@users.noreply.github.com>
2022-02-15 15:26:34 -0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,8 @@
+nombres= ["Arambula", "Anita", "Daniela", "Yeidi", "Gaby", "Rubi", "Mariana", "Juanis", "Sofi", "Nori", "Viliglis", "Jorgito", "Mate", "Jacobo Alejandro", "Jose", "Dani C", "Pacho"]
+edades=[]
+suma= 0
+for (let i=0; i< 17; i= i+1){
+    do (edad=(Number(prompt(`Ingrese la edad ${nombres[i++]}`))){
+        while(isNaN (edad) || edad< 20 || edad> 10)
+    }
+}
