6370c681b47b9a32001ded33670c630ee16a6e1d
Serginator99 <92330120+Serginator99@users.noreply.github.com>
2022-02-22 15:31:08 -0500

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -1,19 +1,16 @@
-nombres= ["Arambula", "Anita", "Daniela", "Yeidi", "Gaby", "Rubi", "Mariana", "Juanis", "Sofi", "Nori", "Viliglis", "Jorgito", "Mate", "Jacobo Alejandro", "Jose", "Dani C", "Pacho"]
+let nombres= ["Arambula", "Anita", "Daniela", "Yeidi", "Gaby", "Rubi", "Mariana", "Juanis", "Sofi", "Nori", "Viliglis", "Jorgito", "Mate", "Jacobo Alejandro", "Jose", "Dani C", "Pacho"]
 edades=[]
 for (let i=0; i< 17; i= i+1){
     do
     {
-        (edad= Number(prompt(`Ingrese la edad de ${nombres[i]}`))
-    }
-    while(isNaN (edad) || edad<20 || edad>10);
+        edad= Number(prompt(`Ingrese la edad de ${nombres[i]}`))
+    }while(isNaN (edad) || edad>20 || edad<10);
     edades.push(edad)
 }
+suma= 0
 for ( let i= 0; i<17; i++){
      suma= suma+edades[i];
 }
 promedio= suma/17
-let contenido= document.querySelector("#informacion")
-informacion.innerHTML= "el Promedio de las notas "+ promedio
-
-
-
+let prom= document.querySelector("#informacion")
+prom.innerHTML = "el Promedio de las notas"+ promedio
