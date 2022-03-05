6094f073fdf78000778b59450dcc802b2a5b0a76
Serginator99 <92330120+Serginator99@users.noreply.github.com>
2022-03-05 16:01:35 -0500

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,40 +1,35 @@
 function piso(nPiso, altura){
-    let nEspacios= altura- nPiso
-    let nNumerales= nPiso
     texto = "<p>"
-    for (i = 0; i< nEspacios; i++){
-        texto = texto + "&nbsp;";
-    }
-
-    if (nPiso == altura){
-        for (i = 0; i< nNumerales; i++){
-                texto= texto + "#";
-        }
-        texto= texto + "</p>"
-        return texto
+     if(nPiso == altura){
+       for(i=0; i<2*altura-1; i++){
+         texto = texto + "#"
+       }
+     }else{
+       nEspaciosIzq = altura-nPiso;
+       nEspaciosDer = 2*nPiso-3;
+    for(i=0;i<nEspaciosIzq; i++){
+        texto = texto + "&nbsp;"
     }
     if(nPiso == 1){
-        texto= texto + "#";
-        texto= texto + "</p>"
-        return texto
+      texto = texto + "#"
+    }else{
+      texto = texto + "#"
+      for(i=0;i< nEspaciosDer;i++){
+        texto = texto + "&nbsp"
+      }
+      texto = texto + "#"
     }
-    if(1< nPiso && nPiso< altura){
-        texto= texto + "#";
-        for (i = 0; i< 2*nPiso-3; i++){
-            texto = texto + "&nbsp;";
-        }
-        texto= texto + "#";
-        texto= texto + "</p>"
-        return texto
-    }
-}
-let nPisos;
-do{
-nPisos = parseInt (prompt("Ingrese la altura de la torre. Número entre 1 y 10"))
-}while(isNaN(nPisos) || nPisos < 1 || nPisos >= 10);// La condición para que se repita
-let  contenido = "";
-for(let i = 1; i <= nPisos; i++){
-    contenido = contenido + piso(i, nPisos)
+   }
+   texto = texto + "</p>"
+    return texto;
+ }
+ let nPisos;
+do {
+    nPisos = Number(prompt("Ingrese el número de pisos que desee"))
+} while(isNaN(nPisos) || nPisos<=0 || nPisos>10 || nPisos%1 !=0)
+let contenido = ""
+for (let i=1; i <= nPisos; i++) {
+    contenido = contenido + piso(i, nPisos);    
 }
 
 let contenedor = document.querySelector("#contenedor");
