63c950be65173403a576a7328cd780b3e8c5272d
Serginator99 <92330120+Serginator99@users.noreply.github.com>
2022-04-19 15:01:11 -0500

    Create Parque final.js

---
+++ Parque final.js
@@ -0,0 +1,36 @@
+Pais = prompt("Ingrese si es de US o de COL")
+
+do{
+    Pais =prompt("Ingrese si es de US o de COL")
+}while(Pais!= "COL" && Pais!= "US");
+if (Pais== "COL"){
+    do {
+        temperatura= Number(prompt("Ingrese su temperatura en Grados"))
+    } while (isNaN(temperatura) || (temperatura>50) || (temperatura<=0));
+    do {
+        estatura= Number(prompt("Ingrese su estatura en centímetros"))
+    } while (isNaN(estatura));
+    do {
+        Acompañante= prompt("¿Va con un acompañante adulto?")
+    } while (Acompañante!= "Si" && Acompañante!= "No")
+    if(estatura<140 && Acompañante== "No" || temperatura>38)
+        respuesta.innerHTML= "No puede entrar"
+        else{
+            respuesta.innerHTML= "Si puede entrar"
+        }
+}else{
+    do {
+        temperatura= Number(prompt("Ingrese su temperatura en Farenheit"))
+    } while (isNaN(temperatura) || (temperatura>122) || (temperatura<=0));
+    do {
+        estatura= Number(prompt("Ingrese su estatura en pies"))
+    } while (isNaN(estatura));
+    do {
+        Acompañante= prompt("¿Va con un acompañante adulto?")
+    } while (Acompañante!= "Si" && Acompañante!= "No");
+    if(estatura<4.5 && Acompañante== "No" || temperatura> 100.4)
+        respuesta.innerHTML= "No puede entrar"
+        else{
+            respuesta.innerHTML= "Si puede entrar"
+        }
+}
